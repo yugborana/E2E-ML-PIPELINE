b09b332a0c5ae0be78eb67992a8dc15d701c5036
Yug Borana <yugborana000@gmail.com>
2025-11-02 17:19:28 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an end to end MLOps Project',
+    author='yb',
+    license='',
+)
